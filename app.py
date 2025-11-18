380808eebc1ebb6af88c091d4dce121bef8dbf54
rahul22106 <rahulkrmishra2004@gmail.com>
2025-11-18 20:04:08 +0530

    updated app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ uploaded_file = st.file_uploader("Upload an MRI image...", type=["jpg", "jpeg", 
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image', use_container_width=True)
     arr = preprocess(image)
     pred_probs = model.predict(arr)[0]
     pred_idx = int(np.argmax(pred_probs))
